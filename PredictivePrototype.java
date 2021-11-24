460e9d3afe57b8953bbf09acad978d1ee09d99e8
Poodoop <55837575+Poodoop@users.noreply.github.com>
2021-11-24 22:17:11 +0800

    Update PredictivePrototype.java

--- PredictivePrototype.java
+++ PredictivePrototype.java
@@ -1,43 +1,89 @@
+package predictive;
+
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+
+
 public class PredictivePrototype{
+	
+	
+	
     public static String wordToSignature(String word){
         StringBuffer strb= new StringBuffer();
+        word = word.toLowerCase();
        int length=word.length();
       for(int i=0; i<length; i++){
           if(word.charAt(i)=='a'||word.charAt(i)=='b'||word.charAt(i)=='c'){
-              strb.append("2");
+           strb.append("2");
           }
           else if(word.charAt(i)=='d'||word.charAt(i)=='e'||word.charAt(i)=='f'){
-              strb.append("3");
+           strb.append("3");
           }
           else if(word.charAt(i)=='g'||word.charAt(i)=='h'||word.charAt(i)=='i'){
            strb.append("4");
-       }
-       else if(word.charAt(i)=='j'||word.charAt(i)=='k'||word.charAt(i)=='l'){
+          }
+          else if(word.charAt(i)=='j'||word.charAt(i)=='k'||word.charAt(i)=='l'){
            strb.append("5");
-       }
-       else if(word.charAt(i)=='m'||word.charAt(i)=='n'||word.charAt(i)=='o'){
+          }
+          else if(word.charAt(i)=='m'||word.charAt(i)=='n'||word.charAt(i)=='o'){
            strb.append("6");
-       }
-       else if(word.charAt(i)=='p'||word.charAt(i)=='q'||word.charAt(i)=='r'||word.charAt(i)=='s'){
+          }
+          else if(word.charAt(i)=='p'||word.charAt(i)=='q'||word.charAt(i)=='r'||word.charAt(i)=='s'){
            strb.append("7");
-       }
-       else if(word.charAt(i)=='t'||word.charAt(i)=='u'||word.charAt(i)=='v'){
+          }
+          else if(word.charAt(i)=='t'||word.charAt(i)=='u'||word.charAt(i)=='v'){
            strb.append("8");
-       }
-       else if(word.charAt(i)=='w'||word.charAt(i)=='x'||word.charAt(i)=='y'||word.charAt(i)=='z'){
-          strb.append("9");
-       }
-       else{
+          }
+          else if(word.charAt(i)=='w'||word.charAt(i)=='x'||word.charAt(i)=='y'||word.charAt(i)=='z'){
+           strb.append("9");
+          }
+          else{
            strb.append(" ");
        }
       }
-       String res=strb.toString();
+       String res = strb.toString();
        return res;
+    }
+    
+    public static Set<String> signatureToWords(String signature) throws FileNotFoundException{
+    	
+    	Set<String> set = new TreeSet<String>();
+    	
+    	
+    	File file = new File("words");
+        Scanner scan = new Scanner(file);
+        
+		while (scan.hasNextLine()) {
+			
+			String i = scan.nextLine();
+			
+			boolean check = isValidWord (i);
+			if (check == false) continue;
+			
+			String sig = PredictivePrototype.wordToSignature(i);
+			if (sig.equals(signature)) {
+				set.add(i);
+			}
+			 scan.close();
+		}
+		return set;
    }
-       public static void main(String[] args){
-   
-       System.out.println(wordToSignature("home"));
-       
-   }
-   }
-   
+
+    private static boolean isValidWord(String word) {
+    	char[] charArray = word.toCharArray();
+        for(char c :charArray){
+            if (!Character.isLetterOrDigit(c)) return false;
+        }
+        return true;
+    	
+    }
+
+    //check output
+    public static void main(String[] args) throws FileNotFoundException{
+    	   
+        System.out.println(wordToSignature("home"));
+        System.out.println(signatureToWords("home"));
+    }
+
+}
