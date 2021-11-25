c3f7c7d1c02e34fc2df18606a430c35596e0922d
Poodoop <55837575+Poodoop@users.noreply.github.com>
2021-11-25 21:39:31 +0800

    Update Words2SigProto.java

--- Words2SigProto.java
+++ Words2SigProto.java
@@ -1,7 +1,11 @@
 //package predictive;
 
 import java.util.*;
-
+//The command line program Words2SigProto will make use of the wordToSignature method from PredictivePrototype.
+//The String[] args array will store all input.
+//If the array is empty, it will use a scanner to read the user input at runtime and add it to the recently made list.
+//If the array is not empty / has an input, it will create a new list with that new input in it.
+//If the new list is not empty, it will use the wordToSignature method and print out the result.
 public class Words2SigProto {
 	
 	public static void main(String[]args) {
